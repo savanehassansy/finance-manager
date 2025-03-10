16f7444327f4fb31efb42c7bf1c9e1de875c0a0c
savanehassansy <savanehassansy@gmail.com>
2025-03-10 13:00:36 +0000

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { SourcesComponent } from './pages/sources/sources.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'dash',
+        component: DashboardComponent
+    }, 
+    {
+        path:'source',
+        component: SourcesComponent
+    }
+];
